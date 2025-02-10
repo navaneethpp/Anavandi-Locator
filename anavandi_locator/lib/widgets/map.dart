83db971518fd607de9933f4be4bd6762603ffcf8
navaneethpp <appunavaneeth0@gmail.com>
2025-02-11 02:29:11 +0530

    Connected the Anavandi Locator to the database and added the real time bus location.Add is is not tested

--- anavandi_locator/lib/widgets/map.dart
+++ anavandi_locator/lib/widgets/map.dart
@@ -1,17 +1,21 @@
-// map.dart
-
-import 'package:anavandi_locator/api/open_route_service.dart';
+import 'dart:convert';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong2/latlong.dart';
 import 'package:geolocator/geolocator.dart';
-import 'constants.dart'; // Import the constants file
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import 'dart:convert';
 import 'package:http/http.dart' as http;
 
+class Coordinates {
+  final double latitude;
+  final double longitude;
+
+  Coordinates(this.latitude, this.longitude);
+}
+
 class MapWidget extends StatefulWidget {
-  const MapWidget({super.key});
+  const MapWidget({Key? key}) : super(key: key);
 
   @override
   State<MapWidget> createState() => _MapWidgetState();
@@ -19,21 +23,17 @@ class MapWidget extends StatefulWidget {
 
 class _MapWidgetState extends State<MapWidget> {
   final MapController _mapController = MapController();
-  LatLng? _userLocation; // Make nullable
-  final LatLng _busCurrentLocation =
-      const LatLng(10.767529113068605, 76.64929215373253);
-  final LatLng _busEndLocation =
-      const LatLng(10.765584666481779, 75.92557352614128);
-  List<LatLng> _routePoints = [];
+  LatLng? _userLocation;
+  LatLng? _busCurrentLocation;
   bool _loadingLocation = false;
-  final String _openRouteServiceApiKey =
-      openRouteSerivceAPI; // Replace with your API key
+  List<LatLng> _polylinePoints = [];
 
   @override
   void initState() {
     super.initState();
     _initializeUserLocation();
-    _fetchDirections();
+    _listenToBusLocation();
+    _fetchRoute();
   }
 
   Future<void> _initializeUserLocation() async {
@@ -42,24 +42,25 @@ class _MapWidgetState extends State<MapWidget> {
     });
 
     try {
-      // Check if location services are enabled
       bool serviceEnabled = await Geolocator.isLocationServiceEnabled();
       if (!serviceEnabled) {
-        bool shouldOpenSettings = await _showEnableGpsDialog();
-        if (shouldOpenSettings) {
-          await Geolocator.openLocationSettings();
-        }
         throw Exception('Location services are disabled.');
       }
 
-      // Check permissions
       LocationPermission permission = await Geolocator.checkPermission();
       if (permission == LocationPermission.denied ||
           permission == LocationPermission.deniedForever) {
-        await Geolocator.requestPermission();
+        permission = await Geolocator.requestPermission();
       }
 
-      // Get current location
+      if (permission == LocationPermission.deniedForever) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(
+              content: Text('Location permission permanently denied.')),
+        );
+        return;
+      }
+
       Position position = await Geolocator.getCurrentPosition(
           desiredAccuracy: LocationAccuracy.high);
 
@@ -69,13 +70,10 @@ class _MapWidgetState extends State<MapWidget> {
         _userLocation = currentLocation;
       });
 
-      // Move map to the user's location
       _mapController.move(currentLocation, 14);
+      print("User Location: $_userLocation");
     } catch (e) {
-      setState(() {
-        _userLocation = null; // Set to null if location is unavailable
-      });
-      _mapController.move(_busCurrentLocation, 14); // Center on bus
+      _mapController.move(LatLng(10.767529, 76.649292), 14);
       ScaffoldMessenger.of(context).showSnackBar(
         SnackBar(content: Text('Error: ${e.toString()}')),
       );
@@ -86,62 +84,82 @@ class _MapWidgetState extends State<MapWidget> {
     }
   }
 
-  Future<bool> _showEnableGpsDialog() async {
-    return await showDialog<bool>(
-          context: context,
-          builder: (context) => AlertDialog(
-            title: const Text('Enable GPS'),
-            content: const Text(
-                'Location services are disabled. Would you like to enable GPS?'),
-            actions: [
-              TextButton(
-                onPressed: () {
-                  Navigator.of(context).pop(false);
-                },
-                child: const Text('Cancel'),
-              ),
-              TextButton(
-                onPressed: () {
-                  Navigator.of(context).pop(true);
-                },
-                child: const Text('Enable'),
-              ),
-            ],
-          ),
-        ) ??
-        false;
+  void _listenToBusLocation() {
+    FirebaseFirestore.instance
+        .collection('location')
+        .doc('location')
+        .snapshots()
+        .listen((snapshot) {
+      if (snapshot.exists) {
+        final data = snapshot.data();
+        if (data != null && data.containsKey('Location')) {
+          final locationString = data['Location'];
+
+          final coordinates = _extractCoordinates(locationString);
+          if (coordinates != null) {
+            setState(() {
+              _busCurrentLocation =
+                  LatLng(coordinates.latitude, coordinates.longitude);
+            });
+          } else {
+            print(
+                "Invalid location string format in Firestore: $locationString");
+          }
+        } else {
+          print("Document or 'Location' field doesn't exist");
+        }
+      } else {
+        print("Snapshot doesn't exist");
+      }
+    });
   }
 
-  Future<void> _fetchDirections() async {
+  Coordinates? _extractCoordinates(String locationString) {
     try {
-      final response = await http.get(
-        Uri.parse(
-            'https://api.openrouteservice.org/v2/directions/driving-car?start=${_busCurrentLocation.longitude},${_busCurrentLocation.latitude}&end=${_busEndLocation.longitude},${_busEndLocation.latitude}'),
-        headers: {'Authorization': _openRouteServiceApiKey},
-      );
+      final parts = locationString.split(RegExp(r',\s*'));
+      if (parts.length != 2) return null;
 
+      final latitudeString = parts[0].replaceAll(RegExp(r'[\u00B0N]'), '');
+      final longitudeString = parts[1].replaceAll(RegExp(r'[\u00B0E]'), '');
+
+      final latitude = double.parse(latitudeString);
+      final longitude = double.parse(longitudeString);
+
+      return Coordinates(latitude, longitude);
+    } catch (e) {
+      print("Error parsing coordinates: $e, String: $locationString");
+      return null;
+    }
+  }
+
+  Future<void> _fetchRoute() async {
+    const String apiKey =
+        '5b3ce3597851110001cf6248319bc5a4a4aa436f943c6ca358396e98';
+    const String apiUrl =
+        'https://api.openrouteservice.org/v2/directions/driving-car';
+    const LatLng startPoint = LatLng(10.765701711281343, 75.92560325817277);
+    const LatLng endPoint = LatLng(10.767729909389303, 76.6493706289483);
+
+    final url = Uri.parse(
+        '$apiUrl?api_key=$apiKey&start=${startPoint.longitude},${startPoint.latitude}&end=${endPoint.longitude},${endPoint.latitude}');
+
+    try {
+      final response = await http.get(url);
       if (response.statusCode == 200) {
         final data = json.decode(response.body);
         final List<dynamic> coordinates =
             data['features'][0]['geometry']['coordinates'];
 
         setState(() {
-          _routePoints = coordinates
-              .map((coord) => LatLng(coord[1], coord[0])) // Convert to LatLng
-              .toList();
+          _polylinePoints =
+              coordinates.map((coord) => LatLng(coord[1], coord[0])).toList();
         });
       } else {
-        throw Exception('Failed to fetch directions: ${response.reasonPhrase}');
+        print('Failed to fetch route: ${response.statusCode}');
       }
     } catch (e) {
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(content: Text('Error fetching directions: ${e.toString()}')),
-      );
+      print('Error fetching route: $e');
     }
-  }
-
-  void _recenterMap(LatLng targetLocation) {
-    _mapController.move(targetLocation, 14);
   }
 
   @override
@@ -149,121 +167,167 @@ class _MapWidgetState extends State<MapWidget> {
     return Scaffold(
       body: Stack(
         children: [
-          FlutterMap(
-            mapController: _mapController,
-            options: MapOptions(
-              initialCenter: _busCurrentLocation, // Default to bus location
-              initialZoom: 11,
-            ),
-            children: [
-              openStreetMapTileLayer,
-              PolylineLayer(
-                polylines: [
-                  Polyline(
-                    points: _routePoints,
-                    strokeWidth: 4.0,
-                    color: Colors.blue,
-                  ),
-                ],
-              ),
-              MarkerLayer(
-                markers: [
-                  // User location (only if available)
-                  if (_userLocation != null)
-                    Marker(
-                      point: _userLocation!,
-                      width: iconSize,
-                      height: iconSize,
-                      alignment: Alignment.center,
-                      child: const Icon(
-                        FontAwesomeIcons.locationDot,
-                        size: iconSize, // Use the constant here
-                        color: Colors.blue,
-                      ),
-                    ),
-
-                  // Bus current Location
-                  Marker(
-                    point: _busCurrentLocation,
-                    width: iconSize,
-                    height: iconSize,
-                    alignment: Alignment.center,
-                    child: Icon(
-                      FontAwesomeIcons.busSimple,
-                      size: iconSize, // Use the constant here
-                      color: Colors.green[700],
-                    ),
-                  ),
-
-                  // Bus End Location
-                  Marker(
-                    point: _busEndLocation,
-                    width: iconSize,
-                    height: iconSize,
-                    alignment: Alignment.center,
-                    child: const Icon(
-                      FontAwesomeIcons
-                          .flagCheckered, // Changed to flag for end location
-                      size: iconSize, // Use the constant here
-                      color: Colors.red,
-                    ),
-                  ),
-                ],
-              ),
-            ],
-          ),
+          _buildMap(),
           if (_loadingLocation)
-            const Center(
-              child: CircularProgressIndicator(),
-            ),
-          Positioned(
-            bottom: 20,
-            left: 10,
-            child: Container(
-              color: Colors.white.withOpacity(0.7),
-              padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
-              child: const Text(
-                'OpenStreetMap Contributors',
-                style: TextStyle(fontSize: 12, color: Colors.black),
-              ),
-            ),
-          ),
+            const Center(child: CircularProgressIndicator()),
         ],
       ),
-      floatingActionButton: PopupMenuButton<String>(
-        onSelected: (value) {
-          if (value == 'user' && _userLocation != null) {
-            _recenterMap(_userLocation!);
-          } else if (value == 'bus') {
-            _recenterMap(_busCurrentLocation);
-          } else if (value == 'fetchLocation') {
-            _initializeUserLocation(); // Fetch current location
-          }
-        },
-        itemBuilder: (context) => [
-          const PopupMenuItem(
-            value: 'user',
-            child: Text('Re-center on User'),
-          ),
-          const PopupMenuItem(
-            value: 'bus',
-            child: Text('Re-center on Bus'),
-          ),
-          const PopupMenuItem(
-            value: 'fetchLocation',
-            child: Text('Fetch Current Location'),
-          ),
-        ],
-        child: const FloatingActionButton(
-          onPressed: null,
-          child: Icon(Icons.menu),
-        ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => _showMenu(context),
+        child: const Icon(Icons.menu),
       ),
     );
   }
-}
 
-TileLayer get openStreetMapTileLayer => TileLayer(
-      urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
-      userAgentPackageName: 'dev.fleaflet.flutter_map.example',
+  void _showMenu(BuildContext context) {
+    showModalBottomSheet(
+      context: context,
+      shape: const RoundedRectangleBorder(
+        borderRadius: BorderRadius.vertical(top: Radius.circular(20)),
+      ),
+      builder: (context) {
+        return Wrap(
+          children: [
+            ListTile(
+              leading: const Icon(FontAwesomeIcons.busSimple),
+              title: const Text('Center Bus Location'),
+              onTap: () {
+                Navigator.pop(context);
+                _centerMapOnBusLocation();
+              },
+            ),
+            ListTile(
+              leading: const Icon(Icons.person_pin),
+              title: const Text('Center User Location'),
+              onTap: () {
+                Navigator.pop(context);
+                _centerMapOnUserLocation();
+              },
+            ),
+            ListTile(
+              leading: const Icon(Icons.refresh),
+              title: const Text('Re-fetch User Location'),
+              onTap: () {
+                Navigator.pop(context);
+                _initializeUserLocation();
+              },
+            ),
+          ],
+        );
+      },
     );
+  }
+
+  void _centerMapOnUserLocation() {
+    if (_userLocation != null) {
+      _mapController.move(_userLocation!, 14);
+    } else {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text('User location is not available.')),
+      );
+    }
+  }
+
+  void _centerMapOnBusLocation() {
+    if (_busCurrentLocation != null) {
+      _mapController.move(_busCurrentLocation!, 14);
+    } else {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text('Bus location is not available.')),
+      );
+    }
+  }
+
+  FutureBuilder<LatLng?> _buildMap() {
+    return FutureBuilder<LatLng?>(
+      future: _getUserOrBusLocation(),
+      builder: (context, snapshot) {
+        LatLng initialCenter = snapshot.data ?? LatLng(10.767529, 76.649292);
+        return FlutterMap(
+          mapController: _mapController,
+          options: MapOptions(
+            initialCenter: initialCenter,
+            initialZoom: 14,
+          ),
+          children: [
+            TileLayer(
+              urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+              userAgentPackageName: 'dev.fleaflet.flutter_map.example',
+            ),
+            _buildMarkers(),
+            if (_polylinePoints.isNotEmpty) _buildPolyline(),
+          ],
+        );
+      },
+    );
+  }
+
+  Widget _buildPolyline() {
+    return PolylineLayer(
+      polylines: [
+        Polyline(
+          points: _polylinePoints,
+          color: Colors.blue,
+          strokeWidth: 4.0,
+        ),
+      ],
+    );
+  }
+
+  Widget _buildMarkers() {
+    List<Marker> markers = [];
+    if (_busCurrentLocation != null) {
+      markers.add(_createBusMarker());
+    }
+    if (_userLocation != null) {
+      markers.add(_createUserMarker());
+    }
+    markers.add(_busEndMark());
+    return MarkerLayer(markers: markers);
+  }
+
+  Marker _createBusMarker() {
+    return Marker(
+      point: _busCurrentLocation!,
+      width: 40,
+      height: 40,
+      child: Icon(
+        Icons.directions_bus,
+        color: Colors.redAccent,
+      ),
+    );
+  }
+
+  Marker _createUserMarker() {
+    return Marker(
+      point: _userLocation!,
+      width: 40,
+      height: 40,
+      child: const Icon(
+        Icons.person_pin_circle,
+        color: Colors.blue,
+      ),
+    );
+  }
+
+  Marker _busEndMark() {
+    return const Marker(
+      point: LatLng(10.765701711281343, 76.647621),
+      width: 40,
+      height: 40,
+      child: (Icon(
+        Icons.flag,
+        color: Colors.green,
+      )),
+    );
+  }
+
+  Future<LatLng?> _getUserOrBusLocation() async {
+    if (_userLocation != null) {
+      return _userLocation;
+    } else if (_busCurrentLocation != null) {
+      return _busCurrentLocation;
+    }
+    return null;
+  }
+}
