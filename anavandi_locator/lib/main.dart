c7b814baf255a773203e2b0a781ed3c79f4bcd89
navaneethpp <appunavaneeth0@gmail.com>
2025-02-15 21:38:19 +0530

    Screen rotation is permanently disabled in the app

--- anavandi_locator/lib/main.dart
+++ anavandi_locator/lib/main.dart
@@ -1,10 +1,13 @@
 import 'package:anavandi_locator/screen/splash_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/services.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
   runApp(const MyApp());
 }
 
