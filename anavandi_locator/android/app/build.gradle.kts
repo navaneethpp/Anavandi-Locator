5a83d9f94445af85ea312bc4f73b0215f94997a9
navaneethpp <appunavaneeth0@gmail.com>
2025-03-30 11:00:34 +0530

    .

--- anavandi_locator/android/app/build.gradle.kts
+++ anavandi_locator/android/app/build.gradle.kts
@@ -25,7 +25,7 @@ android {
         applicationId = "com.example.anavandi_locator"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
+        minSdk = 23 //flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
